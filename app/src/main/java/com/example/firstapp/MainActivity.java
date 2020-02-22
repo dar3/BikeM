d029186b72627989c39690e1064933c8bf3297e7
darko <darkoshypka@gmail.com>
2020-02-22 10:42:33 +0100

    I have added timetable button code to Mainactivity.java, but it doesn't work

--- app/src/main/java/com/example/firstapp/MainActivity.java
+++ app/src/main/java/com/example/firstapp/MainActivity.java
@@ -9,25 +9,55 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
- private Button button;
+    private Button button;
+    private Button button2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d("MainActivity", "onCreate()");
 
-        Log.d("MainActivity", "Test"+"123");
+        Log.d("MainActivity", "Test" + "123");
 
         button = (Button) findViewById(R.id.button2);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 openActivity2();
+
             }
+
+
+            public void openActivity2 (){
+                Intent intent = new Intent(this, Delays.class);
+                startActivity(intent);
+            }
+
         });
-    }
-    public void openActivity2(){
-        Intent intent = new Intent(this, Delays.class);
-        startActivity(intent);
-    }
-}
+        button2 = (Button) findViewById(R.id.button3);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+           openActivity3();
+            }
+            public void  openActivity3(){
+                Intent intent = new Intent(this, Timetable.class);
+                startActivity(intent);
+            }
+
+        });
+
+            }
+            }
+
+
+
+
+
+
+   /* button2  = (Button) findViewById(R.id.button3);
+    button.setonClickListener(new View.OnClickListener)(){
+        @Override
+                public void onClick(View v){}
+    } */
