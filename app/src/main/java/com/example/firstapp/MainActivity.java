1dd1702862ac5e57ec505dfbb576e3e63a862e3a
darko <darkoshypka@gmail.com>
2020-02-22 18:31:16 +0100

    2 buttons added and working. Mar helped

--- app/src/main/java/com/example/firstapp/MainActivity.java
+++ app/src/main/java/com/example/firstapp/MainActivity.java
@@ -9,55 +9,38 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    private Button button;
-    private Button button2;
+    private Button buttonDelay;
+    private Button buttonSchedule;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d("MainActivity", "onCreate()");
 
-        Log.d("MainActivity", "Test" + "123");
-
-        button = (Button) findViewById(R.id.button2);
-        button.setOnClickListener(new View.OnClickListener() {
+        buttonDelay = (Button) findViewById(R.id.buttonDelay);
+        buttonDelay.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                openActivity2();
-
+                openActivityDelays();
             }
-
-
-            public void openActivity2 (){
-                Intent intent = new Intent(this, Delays.class);
-                startActivity(intent);
-            }
-
         });
-        button2 = (Button) findViewById(R.id.button3);
-        button2.setOnClickListener(new View.OnClickListener() {
+
+        buttonSchedule = (Button) findViewById(R.id.buttonSchedule);
+        buttonSchedule.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-           openActivity3();
+                openActivitySchedule();
             }
-            public void  openActivity3(){
-                Intent intent = new Intent(this, Timetable.class);
-                startActivity(intent);
-            }
-
         });
+    }
 
-            }
-            }
+    public void openActivityDelays() {
+        Intent intent = new Intent(this, Delays.class);
+        startActivity(intent);
+    }
 
-
-
-
-
-
-   /* button2  = (Button) findViewById(R.id.button3);
-    button.setonClickListener(new View.OnClickListener)(){
-        @Override
-                public void onClick(View v){}
-    } */
+    public void openActivitySchedule() {
+        Intent intent = new Intent(this, Timetable.class);
+        startActivity(intent);
+    }
+}
